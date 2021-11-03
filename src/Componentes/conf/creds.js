2389daf6ecd42543dacad6bd337ea16acca2f18a
Cesar Emanuel Garcia Perez <57608307+cesargp45@users.noreply.github.com>
2021-11-03 01:24:27 -0600

    [ADD]:https

--- src/Componentes/conf/creds.js
+++ src/Componentes/conf/creds.js
@@ -1,3 +1,3 @@
 //let api_host = 'http://localhost:4000';
-let api_host = 'http://load-balancer-redes2-public-439662731.us-east-2.elb.amazonaws.com';
+let api_host = 'https://www.redes2.ml';
 module.exports = api_host
